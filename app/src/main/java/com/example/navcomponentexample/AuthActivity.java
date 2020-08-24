994d0e21082dac7dfd4d0ce8f2ce61a315e99c12
DhruvLimbachiya <dhruvlimbachiya96870@gmail.com>
2020-08-24 14:24:15 +0530

    Setup ActionBar for handling navigation & displaying titles using AppBarConfiguration.

--- app/src/main/java/com/example/navcomponentexample/AuthActivity.java
+++ app/src/main/java/com/example/navcomponentexample/AuthActivity.java
@@ -1,14 +1,32 @@
 package com.example.navcomponentexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 public class AuthActivity extends AppCompatActivity {
 
+    AppBarConfiguration appBarConfiguration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+
+        NavController  navController = Navigation.findNavController(this,R.id.auth_nav_host);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.auth_nav_host);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
